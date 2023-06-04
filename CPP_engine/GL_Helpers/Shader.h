f61ad7f10731fbefa6e7e4ea378b84b439e1e01d
Lakedemon <leonid.amarandos@gmail.com>
2023-06-04 14:38:26 +0200

    Reorginised merged branch

---
+++ CPP_engine/GL_Helpers/Shader.h
@@ -0,0 +1,25 @@
+#ifndef OPENGLENGINE_SHADER_H
+#define OPENGLENGINE_SHADER_H
+
+#include <glad/glad.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include "../config/config.h"
+
+class Shader {
+public:
+    Shader(const std::string& vsPath, const std::string& fsPath);
+    Shader();
+    [[nodiscard]] unsigned int getId() const;
+private:
+    unsigned int ID;
+    static std::string loadShaderFile(const std::string& path);
+    void initShader(const char* shaderCode, GLenum shaderType) const;
+    static void checkCompile(unsigned int shader);
+    static void checkLinking(unsigned int program);
+};
+
+
+#endif //OPENGLENGINE_SHADER_H
---
+++ CPP_engine/Math/Vector2.cpp
@@ -0,0 +1,65 @@
+#include "Vector2.h"
+
+Vector2::Vector2(float x, float y) : x(x), y(y) {}
+
+float Vector2::magnitude() const {
+    return std::sqrt(x * x + y * y);
+}
+
+Vector2 Vector2::normalized() const {
+    float const length = magnitude();
+    if (length < glMath::EPSILON){
+        return zero;
+    }
+
+    float const lengthInv = 1 / length;
+    return {x * lengthInv, y * lengthInv};
+}
+
+Vector2 Vector2::one = {1, 1};
+Vector2 Vector2::zero = {0, 0};
+Vector2 Vector2::up = {0, 1};
+Vector2 Vector2::down = {0, -1};
+Vector2 Vector2::left = {-1, 0};
+Vector2 Vector2::right = {1, 0};
+
+int Vector2::side(Vector2 depended, Vector2 control, Vector2 normal) {
+    return glMath::sign(dot(depended - control, normal));
+}
+
+float Vector2::distance(Vector2 a, Vector2 b) {
+    return (a - b).magnitude();
+}
+
+float Vector2::angle(Vector2 a, Vector2 b) {
+    float const mag = (a.magnitude() * b.magnitude());
+    if(mag < glMath::EPSILON){return 0;}
+    return std::acos(dot(a, b) / mag);
+}
+
+float Vector2::dot(Vector2 a, Vector2 b) {
+    return a.x * b.x + a.y * b.y;
+}
+
+float Vector2::cross(Vector2 a, Vector2 b) {
+    return a.x * b.y - a.y * b.y;
+}
+Vector2 Vector2::operator+(const Vector2 &b) const {
+    return {x + b.x, y + b.y};
+}
+Vector2 Vector2::operator-(const Vector2 &b) const {
+    return {x - b.x, y - b.y};
+}
+Vector2 Vector2::operator-() const {
+    return {-x, -y};
+}
+Vector2 Vector2::operator*(const Vector2 &b) const {
+    return {x * b.x, y * b.y};
+}
+Vector2 Vector2::operator*(const float &b) const {
+    return {x * b, y * b};
+}
+
+Vector2 Vector2::operator/(const float &b) const {
+    return {x / b, y / b};
+}
