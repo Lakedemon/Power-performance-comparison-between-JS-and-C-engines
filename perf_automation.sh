50d23714e659c6032b946da2e9b3a9a894932296
Leonint Amarantos <76692374+Lakedemon@users.noreply.github.com>
2023-06-09 12:31:30 +0200

    Reverted pref_automation script

--- perf_automation.sh
+++ perf_automation.sh
@@ -3,33 +3,32 @@
 sudo sysctl -w kernel.perf_event_paranoid=0
 cat /proc/sys/kernel/perf_event_paranoid
 
-n=100
-duration_between_runs=60
+n=2
+duration_between_runs=10
 
-
-targets=(
-  "/home/leonint.amarantos/Documents/JS_engine/python3 server.py --scenario Scenario1"
-  "/home/leonint.amarantos/Documents/JS_engine/python3 server.py --scenario Scenario2"
-  "/home/leonint.amarantos/Documents/JS_engine/python3 server.py --scenario Scenario3"
-  "/home/leonint.amarantos/Documents/C++_engine/Scenario1_build/./OpenGLEngine"
-  "/home/leonint.amarantos/Documents/C++_engine/Scenario2_build/./OpenGLEngine"
-  "/home/leonint.amarantos/Documents/C++_engine/Scenario3_build/./OpenGLEngine"
+target_directories=(
+  "/home/leonint.amarantos/Documents/JS_engine"
+  "/home/leonint.amarantos/Documents/JS_engine"
+  "/home/leonint.amarantos/Documents/JS_engine"
+  "/home/leonint.amarantos/Documents/C++_engine/Scenario1_build"
+  "/home/leonint.amarantos/Documents/C++_engine/Scenario2_build"
+  "/home/leonint.amarantos/Documents/C++_engine/Scenario3_build"
 )
 
-new_dir_names=(
-  "JS_low"
-  "JS_mid"
-  "JS_high"
-  "CPP_low"
-  "CPP_mid"
-  "CPP_high"
+targets=(
+  "python3 server.py --scenario Scenario1"
+  "python3 server.py --scenario Scenario2"
+  "python3 server.py --scenario Scenario3"
+  "./OpenGLEngine"
+  "./OpenGLEngine"
+  "./OpenGLEngine"
 )
 
 data_dir="/home/leonint.amarantos/Documents/LA_BSPS2_Data_Collection"
 
 
 for j in "${!targets[@]}"; do
-  mkdir -p $data_dir/${new_dir_names[$j]}
+  mkdir -p $data_dir/target_$j
 
   target="${targets[$j]}"
   target_dir="${target_directories[$j]}"
@@ -38,7 +37,8 @@ for j in "${!targets[@]}"; do
   cd $target_dir
 
   for (( i=1; i<=n; i++ )); do
-    perf stat -a -o $data_dir/${new_dir_names[$j]}/"perf_output_${i}.txt" -e /power/energy-pkg/,/power/energy-ram/ $target
+
+    perf stat -a -o $data_dir/target_$j/"perf_output_${i}.txt" -e /power/energy-pkg/,/power/energy-ram/ $target
     sleep $duration_between_runs
   done
 done
